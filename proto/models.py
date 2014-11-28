d1cdb1e76b5abc4cd5e187a6ac0cd6a6f6dd89fd
caiofsasso <caio_sasso@yahoo.com.br>
2014-11-28 19:48:36 -0200

    Criaçao de modelagem

--- proto/models.py
+++ proto/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
+from django.db import models
+# -*- coding: utf-8 -*-
 from django.db import models
 
-# Create your models here.
+class Aluno (models.Model):
+    nomeUsuario = models.ForeignKey (User)
+    matricula = models.IntegerField(max_length=300)
+
+class Atendente (models.Model):
+    nomeUsuario = models.CharField (User)
+
+class TipoDocumento (models.Model):
+    descricao = models.CharField(max_length=100)
+
+class Documentos (models.Model):
+    tipoDocumento = models.ForeignKey(TipoDocumento)
+    preco = models.FloatField(max_length=6)
+    tempo = models.TimeField()
+    descricao = models.CharField(max_length=2000)
+
+class StatusDocumento (models.Model):
+    status = models.CharField(max_length=25)
+
+class Servico (models.Model):
+    documento = models.ForeignKey(Documentos)
+    aluno = models.ForeignKey(Aluno.matricula)
+    status = models.ForeignKey(StatusDocumento)
+
+
