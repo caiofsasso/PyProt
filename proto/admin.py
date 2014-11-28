220cb4c51d5eaca671408776f11b8254c711497d
caiofsasso <caio_sasso@yahoo.com.br>
2014-11-28 20:59:23 -0200

    Alteraçoes no admin e no models para visualização web.
    Caio, Eduardo, Lucas

--- proto/admin.py
+++ proto/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from proto.models import Aluno, Atendente, TipoDocumento, Documentos, StatusDocumento, Servico
 
-# Register your models here.
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('nomeUsuario', 'matricula')
+
+class AtendenteAdmin(admin.ModelAdmin):
+    list_display = ['nomeUsuario']
+
+class TipoDocumentoAdmin(admin.ModelAdmin):
+    list_display = ['descricao']
+
+class DocumentosAdmin(admin.ModelAdmin):
+    list_display = ('tipoDocumento', 'preco', 'tempo', 'descricao')
+
+class StatusDocumentoAdmin(admin.ModelAdmin):
+    list_display = ['status']
+
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ('documento','aluno', 'status')
+
+admin.site.register(Aluno,AlunoAdmin)
+admin.site.register(Atendente, AtendenteAdmin)
+admin.site.register(TipoDocumento,TipoDocumentoAdmin)
+admin.site.register(Documentos,DocumentosAdmin)
+admin.site.register(StatusDocumento, StatusDocumentoAdmin)
+admin.site.register(Servico, ServicoAdmin)
+
+
+
+
--- proto/models.py
+++ proto/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.db import models
 # -*- coding: utf-8 -*-
 from django.db import models
@@ -7,11 +7,18 @@ class Aluno (models.Model):
     nomeUsuario = models.ForeignKey (User)
     matricula = models.IntegerField()
 
+    def __str__(self):
+        return 'Usuário: {0} - Matrícula: {1}'.format(self.nomeUsuario, self.matricula)
+
 class Atendente (models.Model):
+    grupo = models.ForeignKey(Group)
     nomeUsuario = models.ForeignKey(User)
 
 class TipoDocumento (models.Model):
     descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
 
 class Documentos (models.Model):
     tipoDocumento = models.ForeignKey(TipoDocumento)
@@ -21,6 +28,9 @@ class Documentos (models.Model):
 
 class StatusDocumento (models.Model):
     status = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.status
 
 class Servico (models.Model):
     documento = models.ForeignKey(Documentos)
