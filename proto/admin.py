c0e03d4d1dbfb063ff1fef3dd6368cd663650cbc
eduardomrosa <eduardo@paratestar.com>
2014-12-01 22:48:56 -0200

    Ajustado alguns caminhos

--- proto/admin.py
+++ proto/admin.py
@@ -17,12 +17,12 @@ class StatusDocumentoAdmin(admin.ModelAdmin):
     list_display = ['status']
 
 class ServicoAdmin(admin.ModelAdmin):
-    list_display = ('documento','aluno', 'status')
+    list_display = ('documento', 'aluno', 'status')
 
-admin.site.register(Aluno,AlunoAdmin)
+admin.site.register(Aluno, AlunoAdmin)
 admin.site.register(Atendente, AtendenteAdmin)
-admin.site.register(TipoDocumento,TipoDocumentoAdmin)
-admin.site.register(Documentos,DocumentosAdmin)
+admin.site.register(TipoDocumento, TipoDocumentoAdmin)
+admin.site.register(Documentos, DocumentosAdmin)
 admin.site.register(StatusDocumento, StatusDocumentoAdmin)
 admin.site.register(Servico, ServicoAdmin)
 
